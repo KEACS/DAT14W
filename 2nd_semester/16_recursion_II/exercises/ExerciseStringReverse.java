031f9c578288e65b4857d4ea1357569f5ded6066
Claus Bové <clbo@kea.dk>
2014-10-17 10:29:48 +0200

    re

---
+++ 2nd_semester/16_recursion_II/exercises/ExerciseStringReverse.java
@@ -0,0 +1,18 @@
+package exercises.stringReverse;
+
+public class ExerciseStringReverse
+{
+	/*
+	 * Print an array backwards. Write a recursive method stringReverse that
+	 * takes a character array containing a String as an argument and prints the
+	 * String backwards. Hint: Use the String method toCharArray(), to get a
+	 * char array containing characters in the String
+	 */
+	public static void main(String[] args)
+	{
+		
+
+	}
+
+
+}
